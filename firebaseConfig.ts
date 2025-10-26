f60aece58a04b615cd795536caa03001dca5c084
ANKU890 <84028454+ANKU890@users.noreply.github.com>
2025-10-26 10:54:12 +0530

    Refactor Firebase config to use environment variables

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,11 +1,9 @@
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyDj82u2CaAos9yzbM5ZSmZDK7vIwqYbbio",
-  authDomain: "pump-manager-0069.firebaseapp.com",
-  projectId: "pump-manager-0069",
-  storageBucket: "pump-manager-0069.firebasestorage.app",
-  messagingSenderId: "342442651103",
-  appId: "1:342442651103:web:e8d4653e6c5659aa5ca86a",
-  measurementId: "G-ETF7KPTE7R"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
